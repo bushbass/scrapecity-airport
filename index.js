f28f93eb3f989f916416f29a4721b38d331a3fce
EC2 Default User <ec2-user@ip-172-31-36-64.us-east-2.compute.internal>
2019-04-08 01:11:03 +0000

    up day day 2 9:50, need to still make it return json

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import { getHTML, getArrivalTimes } from "./lib/scraper";
+import express from 'express'
 
-async function go() {
-  getArrivalTimes(await getHTML("https://www.airport-ewr.com/newark-arrivals"));
-}
+const app = express()
 
-go();
+app.get('/scrape', async (req,res,next)=>{
+    console.log('scraping')
+   
+  const countObject = await getArrivalTimes(await getHTML("https://www.airport-ewr.com/newark-arrivals"));
+  
+//   console.log(countObject)
+
+})
+
+
+// async function go() {
+//   const countObject = await getArrivalTimes(await getHTML("https://www.airport-ewr.com/newark-arrivals"));
+//   return countObject
+// }
+
+app.listen (process.env.PORT, ()=> console.log(`app running on ${process.env.PORT}`))
